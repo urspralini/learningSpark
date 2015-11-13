d40569dcdfb483743b8d4c1e2ee2f7d4eb6c4a78
pbabu <prabhu.babu@elementum.com>
2015-11-13 05:36:21 -0800

    spark sql: mysql jdbc example

--- build.sbt
+++ build.sbt
@@ -7,4 +7,5 @@ scalaVersion := "2.11.7"
 libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "1.5.1",
 "javax.servlet" % "javax.servlet-api" % "3.0.1",
 "org.apache.spark" %% "spark-sql" % "1.3.1",
-"org.apache.spark" %% "spark-hive" % "1.3.1")
+"org.apache.spark" %% "spark-hive" % "1.3.1",
+"mysql" % "mysql-connector-java" % "5.1.37")
---
+++ src/main/scala/com/prabhu/spark/basics/JDBCExample.scala
@@ -0,0 +1,42 @@
+package com.prabhu.spark.basics
+
+import java.sql.{ResultSet, DriverManager}
+
+import org.apache.spark.rdd.JdbcRDD
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+ * <p>
+ *
+ * </p>
+ * @author Prabhu R Babu
+ *         (11/13/15 5:13 AM)
+ */
+object JDBCExample {
+
+  def createConnection() = {
+    Class.forName("com.mysql.jdbc.Driver").newInstance()
+    DriverManager.getConnection("jdbc:mysql://localhost/test","root", "root")
+  }
+
+  def extractValues(r: ResultSet) = {
+    (r.getInt(1), r.getString(2))
+  }
+  def main(args: Array[String]) {
+    //initialize spark configuration
+    val conf = new SparkConf().setMaster("local")
+      .setAppName(SparkApp.getClass.getSimpleName)
+    //initialize spark context
+    val sc = new SparkContext(conf)
+    //assume we have a users table with fields (id, name)
+    val data = new JdbcRDD(sc,
+                           createConnection,
+                           "SELECT id, name FROM users where ? <= id and id <= ?",
+                           lowerBound = 1,
+                           upperBound = 3,
+                           numPartitions = 1,
+                           extractValues)
+    data.foreach(result => println(""+ result._1 + "," + result._2))
+  }
+
+}
